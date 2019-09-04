87360b7301e877b1e8fc9edb7b77ad224d9dd156
dhani frezz <odankgokil12345@gmail.com>
2019-09-04 14:08:50 +0700

    add twig

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,8 +18,16 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+    public function __construct() {
+        parent::__construct();
+        $this->load->library(['twig']);
+    }
 	public function index()
 	{
-		$this->load->view('welcome_message');
+        $data = [
+            'namasekolah' => 'Al Furqon',
+        ];
+
+        $this->twig->display('index', $data);
 	}
 }
